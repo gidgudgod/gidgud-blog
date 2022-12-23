e222777da19342720ca6c12b0b29d75a35092406
gidgudgod <collegegideonks@gmail.com>
2022-12-23 23:14:39 +0700

    add progress loading bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,18 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { Session } from 'next-auth';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 interface Props {
   session: Session | null;
 }
+
+NProgress.configure({ showSpinner: true, minimum: 0.1 });
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function App({ Component, pageProps }: AppProps<Props>) {
   return (
